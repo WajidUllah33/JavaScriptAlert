d87db839d003073f5aec9091c4cb79eaf98e7595
Saylani_IT Mass Taining <Saylani_IT Mass Taining>
2017-02-09 10:42:52 +0500

    Start My JavaScript Practic

---
+++ app.js
@@ -0,0 +1,8 @@
+function wajid(){
+alert("Hi , How Are You Mr. Omi Khan.");
+alert('I am Fine Wajidullah. Where you from?');
+alert    ("I m from Pakistan Near Landhi Karachi & Where you from Omi?");
+window.alert('i m from johar. Da Zama');
+alert("laila Che charta Qadama Ki di :D");
+
+}
